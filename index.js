8d43b66136df604c101cc410bff6498eee147fa8
kleencut <jt.ascend@gmail.com>
2019-03-06 15:44:56 +1000

    using continuous recognition async and result to file

--- index.js
+++ index.js
@@ -1,23 +1,31 @@
 "use strict";
+/**
+ *  https://docs.microsoft.com/en-gb/javascript/api/microsoft-cognitiveservices-speech-sdk/?view=azure-node-latest
+ */
 
-// pull in the required packages.
-var sdk = require("microsoft-cognitiveservices-speech-sdk");
-var fs = require("fs");
+const sdk = require("microsoft-cognitiveservices-speech-sdk");
+const fs = require("fs");
+const path = require("path");
+const stringify = require("json-stringify-pretty-compact");
+const DEFAULT_AUDIOFILE = "whatstheweatherlike.wav"; // 16000 Hz, Mono
+const TIMER_ID = 'Timer';
+
 require('dotenv').config()
 
-var DEFAULT_FILE = "whatstheweatherlike.wav"; // 16000 Hz, Mono
-var filename = DEFAULT_FILE;
+var audiofile = DEFAULT_AUDIOFILE;
 var args = process.argv.slice(2);
 
 if (args.length > 0) {
-    filename = args[0]
+    audiofile = args[0]
 }
 
 // start the recognizer and wait for a result.
-if (!fs.existsSync(filename)) {
-    console.log("Audio file not found " + filename);
+if (!fs.existsSync(audiofile)) {    
+    console.log("Audio file not found " + audiofile);
     process.exit(1);
 }
+
+console.time(TIMER_ID);
 
 // replace with your own subscription key,
 // service region (e.g., "westus"), and
@@ -30,14 +38,15 @@ var serviceRegion = process.env.MS_REGION; // e.g., "westus"
 var pushStream = sdk.AudioInputStream.createPushStream();
 
 // open the file and push it to the push stream.
-fs.createReadStream(filename).on('data', function (arrayBuffer) {
+fs.createReadStream(audiofile).on('data', function (arrayBuffer) {
     pushStream.write(arrayBuffer.buffer);
 }).on('end', function () {
     pushStream.close();
 });
 
 // we are done with the setup
-console.log("Now recognizing from file: " + filename);
+console.log("Now recognizing from file: " + audiofile);
+console.time('')
 
 // now create the audio-config pointing to our stream and
 // the speech config specifying the language.
@@ -49,17 +58,65 @@ speechConfig.speechRecognitionLanguage = "en-US";
 
 // create the speech recognizer.
 var recognizer = new sdk.SpeechRecognizer(speechConfig, audioConfig);
+var results = [];
 
-recognizer.recognizeOnceAsync(
-    function (result) {
-        console.log(result);
+recognizer.recognized = (s,e) => {
+  console.log("Final recognition result received. ");
+  results.push(e);
+}
 
-        recognizer.close();
-        recognizer = undefined;
-    },
-    function (err) {
-        console.trace("err - " + err);
+// recognizer.recognizing = (s, e) => {
+//   console.log(`Intermediate ${e.privResult.privText}`);
+// }
 
-        recognizer.close();
-        recognizer = undefined;
+  function genFileName(name, ext = ".json"){
+    if( path.isAbsolute(name)){
+      return (path.basename(name)) + ext;
+    } else {
+      return name + ext;
+    }
+  }
+
+  function saveToFile(filename, content){
+    console.log("Saving to file " + filename);
+    fs.writeFileSync(filename, content,'utf8');
+    console.log("Save complete");
+  }
+
+  function  extractTextFromResults(){
+    let content = "";
+    return results.reduce((text,r) => {
+      return text + r.privResult.privText + "\n";
+    }, "");
+  }
+
+  function saveResultsText(){
+    let content = "";
+    const filename = genFileName(audiofile, ".txt");
+    content = extractTextFromResults();
+    saveToFile(filename, content);
+  }
+
+  function saveResultsJson(){
+    const content = stringify(results, {})
+    const filename = genFileName(audiofile);
+    saveToFile(filename, content);
+  }
+
+recognizer.speechStartDetected = function(s, e){
+  console.log("Speech Start Detected ");
+}
+
+recognizer.speechEndDetected = (s, e) => {
+    console.log("Speech End Detected");    
+    recognizer.stopContinuousRecognitionAsync(() => {
+      console.log("Stop Continuous Recognition");
+      console.timeEnd(TIMER_ID);
+      saveResultsJson();
+      saveResultsText();
+      process.exit(0);
     });
+}
+
+recognizer.startContinuousRecognitionAsync();
+
--- readme.md
+++ readme.md
@@ -1,10 +1,29 @@
 
 # Try microsoft cognitive-services-speech-sdk
 
-The following code is adapted from the [Quickstart: Recognize speech with the Speech SDK for Node.js](https://docs.microsoft.com/en-us/azure/cognitive-services/speech-service/quickstart-js-node) sample from mircosoft.
+##  Configuration
+
+### .env file
+
+Use the [.env.sample](.env.sample) file to create your own local .env file.
 
 
-Microsoft-cognitiveservices-speech-sdk 
+## Run
+To run with the default audio file [whatstheweatherlike.wav](whatstheweatherlike.wav)
+```
+node index.js
+```
+
+To run with a different audio file supply the path as first argument.  Note that at the time of writing Feb 2019 Microsoft-cognitive services speech-sdk only accepted audio formatted as .wav 16000 HZ mono.
+```
+node index.js "/audio-files/some-audio.wav"
+```
+
+## Links
+- The following code is adapted from the [Quickstart: Recognize speech with the Speech SDK for Node.js](https://docs.microsoft.com/en-us/azure/cognitive-services/speech-service/quickstart-js-node) sample from mircosoft.
+
+
+- Microsoft-cognitive services-speech-sdk 
 [API Documentation](
 https://docs.microsoft.com/en-us/javascript/api/overview/azure/speech-service?view=azure-node-latest)
 
